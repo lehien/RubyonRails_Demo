c49d20121577f557bbce14b5770b137b1313e4f3
lehien <hienhyn@gmail.com>
2014-11-23 00:05:22 +0700

    Finish signup

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
 					format: { with: VALID_EMAIL_REGEX },
-					uniqueness: { case-sensitive: false}
+					uniqueness: { case_sensitive: false}
 
 	has_secure_password
 end
