18e8c552dbee45188be6b3fdd512042aa4b4321c
lehien <hienhyn@gmail.com>
2014-11-27 23:31:46 +0700

    process login and logout

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'pages#home'
@@ -14,7 +16,14 @@ Rails.application.routes.draw do
 
   get 'signup' => 'users#new'
 
+  get 'login' => 'sessions#new'
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
+
   resources :users
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
