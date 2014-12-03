ddadbdf4eb4d3aa3901949c2c1043b4c97470fa5
lehien <hienhyn@gmail.com>
2014-12-03 22:12:05 +0700

    add user list and number users per page - 10 users/p

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: "Le Hien",
+	email: "hienhyn@gmail.com",
+	password: "Hien@123",
+	password_confirmation: "Hien@123"
+	)
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@student.hust.edu.vn"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
