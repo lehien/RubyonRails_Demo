d86591388135574433abaf739e4d12a173b76a40
lehien <hienhyn@gmail.com>
2014-12-03 21:24:01 +0700

    showing all users

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :udpate]
   def new
   	@user = User.new
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def create
