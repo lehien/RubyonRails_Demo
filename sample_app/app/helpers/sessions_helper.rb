56fa769e943d03cfea3cc834eadbfb2ec7cd916f
lehien <hienhyn@gmail.com>
2014-12-03 21:01:17 +0700

    add profile edit

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -17,6 +17,9 @@ module SessionsHelper
     end
     end
 
+    def current_user?(user)
+    	user == current_user
+    end
 
 	def logged_in?
 		!current_user.nil?
@@ -38,5 +41,14 @@ module SessionsHelper
 		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
+	end
+
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
 	end
 end
